89b33e9dec3f31b91db70bec33068545e2e68e1b
AJosueBN <anthonyjosue.bn@gmail.com>
2023-06-27 17:12:47 +0000

    Able to source and initialize sql queries

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,4 @@
-INSERT INTO department ()
+INSERT INTO department (id , name) VALUES (1, 'Sales');
+INSERT INTO department (id , name) VALUES (2, 'Software Engineering');
+INSERT INTO department (id , name) VALUES (3, 'Finance');
+INSERT INTO department (id , name) VALUES (4, 'Legal');
