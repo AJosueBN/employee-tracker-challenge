0ad43626c2db73c55283b2244a9d73283e5c8d31
AJosueBN <anthonyjosue.bn@gmail.com>
2023-06-27 17:07:14 +0000

    Able to source sql database

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const inquirer = require('inquirer');
-const {}
+const mysql = require('mysql')
+const {MainMenuQuestions , AddDepartmentQuestions, AddEmployeeQuestions , AddRoleQuestions, UpdateEmployeeRoleQuestions};
+
+
+const doMenuQuestions = () => {
+
+    inquirer.prompt(MainMenuQuestions)
+    .then((response)  => {
+        switch(response.option) {
+            case 'view_departments':
+            view_departments();
+            break;
+            case 'view_roles':
+            view_roles();
+            break;
+            case 'view_employees':
+            view_employees();
+            break;
+            case 'add_department':
+            add_department();
+            break;
+            case 'add_role':
+            add_role();
+            break;
+            case 'add_employee':
+            add_employee();
+            break;
+            case 'update_role':
+            update_role();
+            break;
+            
+        }
+    })
+}
--- questions.js
+++ questions.js
@@ -84,7 +84,7 @@ const UpdateEmployeeRoleQuestions = [
     },
 ]
 
-module.exports = {MainMenuQuestions , AddDepartmentQuestions, AddEmployeeQuestions , UpdateEmployeeRoleQuestions }
+module.exports = {MainMenuQuestions , AddDepartmentQuestions, AddEmployeeQuestions , AddRoleQuestions, UpdateEmployeeRoleQuestions }
 
 
 
